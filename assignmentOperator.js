066784e5ffac3011a51e1757cdca6c94c879231c
programmerTowsif <programmer.towsif@gmail.com>
2025-04-18 12:56:18 +0600

    assignment

---
+++ assignmentOperator.js
@@ -0,0 +1,53 @@
+// let a = 10;
+// a += 5;
+// console.log(a)
+// a-=3;
+// console.log(a)
+// a*=2;
+// console.log(a)
+// a/=4;
+// console.log(a)
+// a%=5;
+// console.log(a)
+// a++;
+// console.log(a)
+// ++a;
+// console.log(a)
+// a--;
+// console.log(a)
+// --a;
+// console.log(a)
+
+// let num = 12;
+// let a = num++;
+// let b = ++num;
+// let c = num--;
+// let d = --num;
+// console.log(a,b,c,d)
+// 🔹 3. Assignment + Increment Combo Challenge
+
+// let salary = 1000;
+// let bonus = 2000;
+// salary +=bonus;
+// console.log(salary)
+// let a = salary++;
+// console.log(a)
+// console.log(salary)
+// let b = salary--;
+// console.log(b,salary)
+
+// let a = 5;
+// a+=3;
+// a*=2;
+// a-=4;
+// console.log(a)
+
+// let x = 10;
+// let y = x++;
+// console.log(x,y)
+
+
+let n = 7;
+let result = ++n + n++ + --n;
+ 
+console.log(result)
